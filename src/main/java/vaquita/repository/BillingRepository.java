8bc58c56f92f062e7305b8fb3372008480d51cbd
nyrwana <nyrwanamusic@gmail.com>
2021-12-09 12:26:50 +0530

    Add Billing, Events, Feedback, Userrequest Repositories

---
+++ src/main/java/vaquita/repository/BillingRepository.java
@@ -0,0 +1,12 @@
+package vaquita.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import vaquita.entity.Passport;
+
+@Repository
+public interface BillingRepository extends JpaRepository<Billing, Integer>
+{
+
+}
